5fea0e17781a6076d6b932d57e60995279d2af9c
agatha-echa <agatha.chaves.souza@escola.pr.gov.br>
2025-10-23 11:59:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+
+const form = document.getElementsById("form")
+const username = document.getElementById("username")
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+const passwordConfirmation = document.getElementById("password-confirmation");
+
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  checkForm();
+})
+
+email.addEventListener("blur", () => {
+  checkInputEmail();
+})
+
+
+username.addEventListener("blur", () => {
+    checkInputUsername();
+  })
+  
+  
+  function checkInputUsername(){
+    const usernameValue = username.value;
+  
+    if(usernameValue === ""){
+      errorInput(username, "Preencha um username!")
+    }else{
+        const formItem = username.parentElement;
+    formItem.className = "form-content"
+  }
+
+}
+
+function checkInputEmail(){
+    const emailValue = email.value;
+
+    if(emailValue === ""){
+        errorInput(email, "O email é obrigatório.")
+      }else{
+        const formItem = email.parentElement;
+        formItem.className = "form-content"
+      }
+
+
+    }
+
+
+    function checkIputPassworld(){
+        const passworldvalue = passworld.value;
+
+        if(passwordValue === ""){
+            errorInput(password, "A senha é obrigatória.")
+          }else if(passwordValue.length < 8){
+            errorInput(password, "A senha precisa ter no mínimo 8 caracteres.")
+          }else{
+            const formItem = password.parentElement;
+            formItem.className = "form-content"
+          }
+
+
+        }
+
+
+        
+        </div>
+  
+
+  <script src="./script.js"></script>
+</body>
+</html>
